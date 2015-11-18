fdc239c007088a5346bdbc9b9122af31b0b1e65c
Emile <emile.swarts123@gmail.com>
2015-11-18 19:01:16 +0000

    Add elpa packages, colour schemes etc.

---
+++ site-lisp/matching-bracket.el
@@ -0,0 +1,5 @@
+(defun goto-match-paren (arg)
+  (interactive "p")
+  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+        (t (self-insert-command (or arg 1)))))
